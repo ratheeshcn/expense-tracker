ad423bc8bc82055066b5c5461fb8378bceeb38b7
Ratheesh CN <ratheeshcn87@gmail.com>
2024-08-12 15:36:09 +0400

    added manage expenses buttons

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,5 +1,10 @@
-import { Text } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 import { useLayoutEffect } from "react";
+import IconButton from "../components/ui/IconButton";
+import { Colors } from "react-native/Libraries/NewAppScreen";
+import { GlobalStyles } from "../constants/styles";
+import Button from "../components/ui/Button";
+import { StackRouter } from "@react-navigation/native";
 
 function ManageExpense({ route, navigation }) {
   const editedExpenseId = route.params?.expenseId;
@@ -10,7 +15,63 @@ function ManageExpense({ route, navigation }) {
     });
   }, [navigation.isEditing]);
 
-  return <Text>Manage Expense Screen</Text>;
+  function deleteExpenseHandler() {
+    navigation.goBack();
+  }
+  function confirmHandler() {
+    navigation.goBack();
+  }
+
+  function cancelHandler() {
+    navigation.goBack();
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.buttons}>
+        <Button style={styles.button} mode="flat" onPress={cancelHandler}>
+          Cancel
+        </Button>
+        <Button style={styles.button} onPress={confirmHandler}>
+          {isEditing ? "Update" : "Add"}
+        </Button>
+      </View>
+      {isEditing && (
+        <View style={styles.deleteContainer}>
+          <IconButton
+            icon="trash"
+            color={GlobalStyles.Colors.error500}
+            size={36}
+            onPress={deleteExpenseHandler}
+          />
+        </View>
+      )}
+    </View>
+  );
 }
 
 export default ManageExpense;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.Colors.primary800,
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8,
+  },
+  deleteContainer: {
+    marginTop: 16,
+    paddingTop: 8,
+    borderTopWidth: 2,
+    borderTopColor: GlobalStyles.Colors.primary200,
+    alignItems: "center",
+  },
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
